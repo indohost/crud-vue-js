4c9df980159eee27bb6fe8de7c930b0519f08575
indohost <asyarif644@gmail.com>
2021-07-05 05:40:00 +0700

    Vue JS Create CRUD

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,152 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <div class="ui fixed inverted menu vue-color">
+      <div class="ui container">
+        <a href="#" class="header item"> Vue JS CRUD with Laravel API </a>
+      </div>
+    </div>
+
+    <div class="ui main container">
+      <MyForm :form="form" @onFormSubmit="onFormSubmit" />
+      <Loader v-if="loader" />
+      <CustomerList
+        :customers="customers"
+        @onDelete="onDelete"
+        @onEdit="onEdit"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
+import MyForm from "./components/MyForm";
+import CustomerList from "./components/CustomerList";
+import Loader from "./components/Loader";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    MyForm,
+    CustomerList,
+    Loader,
+  },
+  data() {
+    return {
+      url: "http://localhost:8000/api/customers",
+      customers: [],
+      form: {
+        first_name: "",
+        last_name: "",
+        email: "",
+        isEdit: false,
+      },
+      loader: false,
+    };
+  },
+  methods: {
+    getCustomers() {
+      this.loader = true;
+
+      axios.get(this.url).then((data) => {
+        this.customers = data.data;
+        this.loader = false;
+      });
+    },
+    deleteCustomer(id) {
+      this.loader = true;
+
+      axios
+        .delete(`${this.url}/${id}`)
+        .then(() => {
+          this.getCustomers();
+        })
+        .catch((e) => {
+          alert(e);
+        });
+    },
+    createCustomer(data) {
+      this.loader = true;
+
+      axios
+        .post(this.url, {
+          first_name: data.first_name,
+          last_name: data.last_name,
+          email: data.email,
+        })
+        .then(() => {
+          this.getCustomers();
+        })
+        .catch((e) => {
+          alert(e);
+        });
+    },
+    editCustomer(data) {
+      this.loader = true;
+
+      axios
+        .put(`${this.url}/${data.id}`, {
+          first_name: data.first_name,
+          last_name: data.last_name,
+          email: data.email,
+        })
+        .then(() => {
+          this.getCustomers();
+        })
+        .catch((e) => {
+          alert(e);
+        });
+    },
+    onDelete(id) {
+      // window.console.log("app delete " + id);
+      this.deleteCustomer(id);
+    },
+    onEdit(data) {
+      // window.console.log("add edit ", data);
+      this.form = data;
+      this.form.isEdit = true;
+    },
+    onFormSubmit(data) {
+      // window.console.log("app onFormSubmit", data);
+
+      if (data.isEdit) {
+        // call edit customer
+        this.editCustomer(data);
+      } else {
+        // call create customer
+        this.createCustomer(data);
+      }
+    },
+  },
+  created() {
+    this.getCustomers();
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+.vue-color {
+  background: #41b883 !important;
+}
+
+.main.container {
   margin-top: 60px;
+}
+
+.submit-button {
+  margin-top: 24px !important;
+  float: right;
+}
+
+.data {
+  margin-top: 15px;
+}
+
+thead tr th {
+  background: #e0e0e0 !important;
+}
+
+.ui.inverted.dimmer {
+  background-color: rgba(255, 255, 255, 0) !important;
 }
 </style>
